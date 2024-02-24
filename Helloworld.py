9d2b21b4468384189306dd395268308fec51f112
Boris <bvg99@mail.ru>
2024-02-24 09:43:21 +0700

    24.02.2024

---
+++ Helloworld.py
@@ -0,0 +1,9 @@
+def greeting_1():
+    print('Hello, world 1')
+
+# greeting_1()
+
+def greeting_2():
+    print('Hello, world 2')
+
+# greeting_2()
---
+++ main.py
@@ -0,0 +1,5 @@
+from Helloworld import greeting_1 as hi_1
+from Helloworld import greeting_2 as hi_2
+
+hi_1()
+hi_2()
